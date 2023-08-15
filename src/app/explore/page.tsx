0b265a7b1c3926f0b2023292ebe66d191c940d8d
Sundar Nambuvel <sundarnambuvel@Sundars-Air.attlocal.net>
2023-08-14 21:20:43 -0500

    langchain stuff

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -7,30 +7,57 @@ import Link from "next/link";
 import { Input } from "@/components/ui/input"
 import { useState } from "react";
 import BasicFlow from "@/components/explorer/Flow";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 import axios from 'axios';
+import { Textarea } from "@/components/ui/textarea";
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const Page = () => {
     
     const [exploreTopic, setExploreTopic] = useState<string>("")
+    const [rawDataJson, setRawDataJson] = useState<string>("");
 
     const handleExplore = async() => {
       
       const response = await axios.post("/api/explore", {explore:exploreTopic})
-      console.log(JSON.stringify(response, null, 2))
+      setRawDataJson( JSON.stringify(response, null, 2))
+
     }
     
     return (
-    <section className="container flex flex-col gap-y-16">
-      <div className="w-full mt-16 flex gap-x-4 ">
+    <section className="container flex flex-col gap-y-8">
+      <div className="w-full mt-8 flex gap-x-4 ">
         <Input className="flex-1" onChange={ (e) => setExploreTopic(e.target.value) } placeholder="What do you want to Explore!" value={exploreTopic}/>
         <Button onClick={handleExplore}>Explore</Button>        
 
       </div>
 
-      <div className="w-full h-screen">
-        <BasicFlow />
-      </div>
+      
+      <Tabs defaultValue="explore" className="w-full mx-auto">
+      
+        <TabsList className="mx-auto">
+          <TabsTrigger value="explore">Explore</TabsTrigger>
+          <TabsTrigger value="rawdata">RawData</TabsTrigger>
+        </TabsList>
+  
+        <TabsContent value="explore">
+          <ScrollArea className="h-screen" >
+          <div className="w-full h-[calc(100vh-150px)]">
+          <BasicFlow />
+          </div>
+          </ScrollArea>
+        </TabsContent>
+
+        <TabsContent value="rawdata">
+          <Textarea value={rawDataJson} readOnly className="h-[calc(100vh-150px)]"/>
+        </TabsContent>
+    </Tabs>
+    
+
+       
+      
 
       
     </section>
--- src/features/explore/explore-service.ts
+++ src/features/explore/explore-service.ts
@@ -1,10 +1,29 @@
-'use server'
+
 import 'server-only'
+
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage, ChatMessage, SystemMessage } from "langchain/schema"; 
+
+
+const chatAI = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_KEY,
+});
 
 export const ExploreTopic = async ( {explore, context}:
     { explore: string, context : object | null }) => {
-    
-    return {
-        explore, context
+    try{
+        const humanMessage = new HumanMessage( explore )
+        const systemMessage = new SystemMessage("You will help explore the topic provided in the message. You will return data in JSON with each topic and summary of the topic and additional questions related to the topic")
+        const result = await chatAI.predictMessages([systemMessage, humanMessage])
+        console.log(result)
+        return {
+            result
+        }
+    }catch(error){
+        return {
+            "error" : error
+            
+        }
     }
+    
 }
